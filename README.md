1d5a27ca6ec36ff1144e369bf87851be9705a77d
Rei Ryuki <46316533+reiryuki@users.noreply.github.com>
2024-11-08 06:28:55 +0700

    v0.1

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# Miui-Gallery-AI-Magisk-Module
+# Miui Gallery AI Magisk Module
+
+## DISCLAIMER
+- Miui apps are owned by Xiaomi™.
+- The MIT license specified here is for the Magisk Module only, not for Miui apps.
+
+## Descriptions
+- Gallery app by Xiaomi Inc. ported and integrated as a Magisk Module for all supported and rooted devices with Magisk
+- The apk is unmodified because modifying the apk will break the Google Photos Backup feature
+
+## Sources
+- https://apkmirror.com com.miui.gallery by Xiaomi Inc.
+- libmagiskpolicy.so: Kitsune Mask R6687BB53
+
+## Screenshots
+- https://t.me/ryukimodsscreenshots/12
+
+## Requirements
+- arm64-v8a or armeabi-v7a architecture
+- Android 8.0 (SDK 26) and up
+- Magisk or KernelSU installed
+
+## Installation Guide & Download Link
+- Install Miui Core Magisk Module first if you are in non-Miui ROM: https://github.com/reiryuki/Miui-Core-Magisk-Module
+- Install this module https://www.pling.com/p/2221565/ via Magisk app or KernelSU app or Recovery if Magisk installed
+- Install Miui Gallery Editor Magisk Module: https://github.com/reiryuki/Miui-Gallery-Editor-Magisk-Module
+- If you are using KernelSU, you need to disable Unmount Modules by Default in KernelSU app settings
+- Reboot
+- If you are using KernelSU, you need to allow superuser list manually all package name listed in package.txt (and your home launcher app also) (enable show system apps) and reboot afterwards
+- If you are using SUList, you need to allow list manually your home launcher app (enable show system apps) and reboot afterwards
+- Go to app info of Gallery and Gallery Editor app and allow the network access to be able to download the online features
+
+## Optionals
+- https://t.me/androidryukimodsdiscussions/54012
+- Global: https://t.me/ryukinotes/35
+
+## Troubleshootings
+- https://t.me/androidryukimodsdiscussions/64467
+- Global: https://t.me/ryukinotes/34
+
+## Support & Bug Report
+- https://t.me/androidryukimodsdiscussions/2618
+- If you don't do above, issues will be closed immediately
+
+## Credits and Contributors
+- https://t.me/androidryukimodsdiscussions
+- You can contribute ideas about this Magisk Module here: https://t.me/androidappsportdevelopment
+
+## Sponsors
+- https://t.me/ryukinotes/25
+
+
---
+++ customize.sh
@@ -0,0 +1,316 @@
+# space
+ui_print " "
+
+# var
+UID=`id -u`
+[ ! "$UID" ] && UID=0
+ABILIST=`grep_get_prop ro.product.cpu.abilist`
+if [ ! "$ABILIST" ]; then
+  ABILIST=`grep_get_prop ro.system.product.cpu.abilist`
+fi
+
+# log
+if [ "$BOOTMODE" != true ]; then
+  FILE=/data/media/"$UID"/$MODID\_recovery.log
+  ui_print "- Log will be saved at $FILE"
+  exec 2>$FILE
+  ui_print " "
+fi
+
+# optionals
+OPTIONALS=/data/media/"$UID"/optionals.prop
+if [ ! -f $OPTIONALS ]; then
+  touch $OPTIONALS
+fi
+
+# debug
+if [ "`grep_prop debug.log $OPTIONALS`" == 1 ]; then
+  ui_print "- The install log will contain detailed information"
+  set -x
+  ui_print " "
+fi
+
+# recovery
+if [ "$BOOTMODE" != true ]; then
+  MODPATH_UPDATE=`echo $MODPATH | sed 's|modules/|modules_update/|g'`
+  rm -f $MODPATH/update
+  rm -rf $MODPATH_UPDATE
+fi
+
+# run
+. $MODPATH/function.sh
+
+# info
+MODVER=`grep_prop version $MODPATH/module.prop`
+MODVERCODE=`grep_prop versionCode $MODPATH/module.prop`
+ui_print " ID=$MODID"
+ui_print " Version=$MODVER"
+ui_print " VersionCode=$MODVERCODE"
+if [ "$KSU" == true ]; then
+  ui_print " KSUVersion=$KSU_VER"
+  ui_print " KSUVersionCode=$KSU_VER_CODE"
+  ui_print " KSUKernelVersionCode=$KSU_KERNEL_VER_CODE"
+  sed -i 's|#k||g' $MODPATH/post-fs-data.sh
+else
+  ui_print " MagiskVersion=$MAGISK_VER"
+  ui_print " MagiskVersionCode=$MAGISK_VER_CODE"
+fi
+ui_print " "
+
+# architecture
+if [ "$ABILIST" ]; then
+  ui_print "- $ABILIST architecture"
+  ui_print " "
+fi
+NAME=arm64-v8a
+NAME2=armeabi-v7a
+if ! echo "$ABILIST" | grep -q $NAME; then
+  if echo "$ABILIST" | grep -q $NAME2; then
+    rm -rf `find $MODPATH/system -type d -name *64*`
+  else
+    if [ "$BOOTMODE" == true ]; then
+      ui_print "! This ROM doesn't support $NAME nor $NAME2 architecture"
+    else
+      ui_print "! This Recovery doesn't support $NAME nor $NAME2 architecture"
+      ui_print "  Try to install via Magisk app instead"
+    fi
+    abort
+  fi
+fi
+if ! echo "$ABILIST" | grep -q $NAME2; then
+  rm -rf $MODPATH/system*/lib\
+   $MODPATH/system*/vendor/lib
+  if [ "$BOOTMODE" != true ]; then
+    ui_print "! This Recovery doesn't support $NAME2 architecture"
+    ui_print "  Try to install via Magisk app instead"
+    ui_print " "
+  fi
+fi
+
+# sdk
+NUM=26
+if [ "$API" -lt $NUM ]; then
+  ui_print "! Unsupported SDK $API."
+  ui_print "  You have to upgrade your Android version"
+  ui_print "  at least SDK $NUM to use this module."
+  abort
+else
+  ui_print "- SDK $API"
+  ui_print " "
+fi
+
+# check
+if [ ! -d /data/adb/modules/MiuiCore ]; then
+  ui_print "! Miui Core Magisk Module is not installed."
+  ui_print "  This module will not be working without"
+  ui_print "  Miui Core Magisk Module except you are in Miui ROM."
+  ui_print "  Please read the Github README installation guide!"
+  ui_print " "
+else
+  rm -f /data/adb/modules/MiuiCore/remove
+  rm -f /data/adb/modules/MiuiCore/disable
+fi
+
+# check
+if [ ! -d /data/adb/modules/MiuiGalleryEditor ]; then
+  ui_print "! Miui Gallery Editor Magisk Module is not installed."
+  ui_print "  Edit option in Miui Gallery requires"
+  ui_print "  Miui Gallery Editor Magisk Module."
+  ui_print "  Please read the Github README installation guide!"
+  ui_print " "
+fi
+
+# sepolicy
+FILE=$MODPATH/sepolicy.rule
+DES=$MODPATH/sepolicy.pfsd
+if [ "`grep_prop sepolicy.sh $OPTIONALS`" == 1 ]\
+&& [ -f $FILE ]; then
+  mv -f $FILE $DES
+fi
+
+# cleaning
+ui_print "- Cleaning..."
+#PKGS=`cat $MODPATH/package.txt`
+#if [ "$BOOTMODE" == true ]; then
+#  for PKG in $PKGS; do
+#    FILE=`find /data/app -name *$PKG*`
+#    if [ "$FILE" ]; then
+#      RES=`pm uninstall $PKG 2>/dev/null`
+#    fi
+#  done
+#fi
+remove_sepolicy_rule
+ui_print " "
+
+# function
+conflict() {
+for NAME in $NAMES; do
+  DIR=/data/adb/modules_update/$NAME
+  if [ -f $DIR/uninstall.sh ]; then
+    sh $DIR/uninstall.sh
+  fi
+  rm -rf $DIR
+  DIR=/data/adb/modules/$NAME
+  rm -f $DIR/update
+  touch $DIR/remove
+  FILE=/data/adb/modules/$NAME/uninstall.sh
+  if [ -f $FILE ]; then
+    sh $FILE
+    rm -f $FILE
+  fi
+  rm -rf /metadata/magisk/$NAME\
+   /mnt/vendor/persist/magisk/$NAME\
+   /persist/magisk/$NAME\
+   /data/unencrypted/magisk/$NAME\
+   /cache/magisk/$NAME\
+   /cust/magisk/$NAME
+done
+}
+
+# conflict
+NAMES=MIUIGallery
+conflict
+
+# function
+cleanup() {
+if [ -f $DIR/uninstall.sh ]; then
+  sh $DIR/uninstall.sh
+fi
+DIR=/data/adb/modules_update/$MODID
+if [ -f $DIR/uninstall.sh ]; then
+  sh $DIR/uninstall.sh
+fi
+}
+
+# cleanup
+DIR=/data/adb/modules/$MODID
+FILE=$DIR/module.prop
+PREVMODNAME=`grep_prop name $FILE`
+if [ "`grep_prop data.cleanup $OPTIONALS`" == 1 ]; then
+  sed -i 's|^data.cleanup=1|data.cleanup=0|g' $OPTIONALS
+  ui_print "- Cleaning-up $MODID data..."
+  cleanup
+  ui_print " "
+#elif [ -d $DIR ]\
+#&& [ "$PREVMODNAME" != "$MODNAME" ]; then
+#  ui_print "- Different module name is detected"
+#  ui_print "  Cleaning-up $MODID data..."
+#  cleanup
+#  ui_print " "
+fi
+
+# function
+permissive_2() {
+sed -i 's|#2||g' $MODPATH/post-fs-data.sh
+}
+permissive() {
+FILE=/sys/fs/selinux/enforce
+SELINUX=`cat $FILE`
+if [ "$SELINUX" == 1 ]; then
+  if ! setenforce 0; then
+    echo 0 > $FILE
+  fi
+  SELINUX=`cat $FILE`
+  if [ "$SELINUX" == 1 ]; then
+    ui_print "  Your device can't be turned to Permissive state."
+    ui_print "  Using Magisk Permissive mode instead."
+    permissive_2
+  else
+    if ! setenforce 1; then
+      echo 1 > $FILE
+    fi
+    sed -i 's|#1||g' $MODPATH/post-fs-data.sh
+  fi
+else
+  sed -i 's|#1||g' $MODPATH/post-fs-data.sh
+fi
+}
+
+# permissive
+if [ "`grep_prop permissive.mode $OPTIONALS`" == 1 ]; then
+  ui_print "- Using device Permissive mode."
+  rm -f $MODPATH/sepolicy.rule
+  permissive
+  ui_print " "
+elif [ "`grep_prop permissive.mode $OPTIONALS`" == 2 ]; then
+  ui_print "- Using Magisk Permissive mode."
+  rm -f $MODPATH/sepolicy.rule
+  permissive_2
+  ui_print " "
+fi
+
+# function
+extract_lib() {
+for APP in $APPS; do
+  FILE=`find $MODPATH/system -type f -name $APP.apk`
+  if [ -f `dirname $FILE`/extract ]; then
+    ui_print "- Extracting..."
+    DIR=`dirname $FILE`/lib/"$ARCHLIB"
+    mkdir -p $DIR
+    rm -rf $TMPDIR/*
+    DES=lib/"$ABILIB"/*
+    unzip -d $TMPDIR -o $FILE $DES
+    cp -f $TMPDIR/$DES $DIR
+    ui_print " "
+  fi
+done
+}
+hide_oat() {
+for APP in $APPS; do
+  REPLACE="$REPLACE
+  `find $MODPATH/system -type d -name $APP | sed "s|$MODPATH||g"`/oat"
+done
+}
+
+# extract
+APPS="`ls $MODPATH/system/priv-app`
+      `ls $MODPATH/system/app`"
+ARCHLIB=arm64
+ABILIB=arm64-v8a
+extract_lib
+ARCHLIB=arm
+if echo "$ABILIST" | grep -q armeabi-v7a; then
+  ABILIB=armeabi-v7a
+  extract_lib
+elif echo "$ABILIST" | grep -q armeabi; then
+  ABILIB=armeabi
+  extract_lib
+else
+  ABILIB=armeabi-v7a
+  extract_lib
+fi
+ARCHLIB=x64
+ABILIB=x86_64
+extract_lib
+ARCHLIB=x86
+ABILIB=x86
+extract_lib
+rm -f `find $MODPATH/system -type f -name extract`
+# hide
+hide_oat
+
+# features
+PROP=`grep_prop miui.features $OPTIONALS`
+FILE=$MODPATH/system.prop
+if [ "$PROP" == 1 ]; then
+  ui_print "- ro.product.device will be changed to cepheus"
+  ui_print "  ro.product.manufacturer will be changed to Xiaomi"
+  ui_print "  It may break some system apps and features functionality"
+  sed -i "s|#ro.product|ro.product|g" $FILE
+  ui_print " "
+elif [ "$PROP" ] && [ "$PROP" != 0 ]; then
+  ui_print "- ro.product.device will be changed to $PROP"
+  ui_print "  ro.product.manufacturer will be changed to Xiaomi"
+  ui_print "  It may break some system apps and features functionality"
+  sed -i "s|#ro.product|ro.product|g" $FILE
+  sed -i "s|cepheus|$PROP|g" $FILE
+  ui_print " "
+fi
+
+
+
+
+
+
+
+
